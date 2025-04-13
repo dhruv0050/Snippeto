6bb35c21e62c2284386660731f3522797567fe29
Dhruv Sharma <dhruvsh5467@gmail.com>
2025-04-13 11:46:06 +0530

    data creation in db,  fetch and display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+
+  const snippets = await prisma.snippeto.findMany()
+
   return (
     <div>
       <div className="flex items-center justify-between">
         <h1>Snippeto</h1>
         <Link href={"/snippet/new"}><Button>New</Button></Link>
       </div>
+      {
+        snippets.map((snippet) => (
+          <div key={snippet.id} className="flex items-center justify-between bg-gray-200 p-2 rounded-md my-2">
+            <h1>{snippet.title}</h1>
+            <Link href={`/snippet/{snippet.id}`}><Button variant={'link'}>View</Button></Link>
+          </div>
+        ))
+      }
     </div>
   );
 }
--- src/app/snippet/new/page.tsx
+++ src/app/snippet/new/page.tsx
@@ -2,18 +2,37 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
+import { prisma } from '@/lib/prisma'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 const CreateSnippetPage = () => {
+
+    async function createSnippet(formData: FormData) {
+        'use server'
+        const title = formData.get('title') as string
+        const code = formData.get('code') as string
+
+       const snippet = await prisma.snippeto.create({     //Snippeto = model name
+        data:{
+            title,
+            code
+        }
+       })
+       
+       console.log(snippet)
+
+       redirect('/')
+    }
     return (
-        <form>
+        <form action={createSnippet}>
             <div>
                 <Label>Title</Label>
                 <Input type="text" name='title' id='title' />
             </div>
             <div>
                 <Label>Code</Label>
-                <Textarea name='title' id='title' />
+                <Textarea name='code' id='code' />
             </div>
             <Button type='submit' className='my-4'>New</Button>
         </form>
